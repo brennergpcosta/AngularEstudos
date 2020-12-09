9a8b068bf106b50eb95c5adf91e0ab592e152c67
brennergpcosta <brennergpcosta@gmail.com>
2020-12-09 12:18:41 -0300

    Ended Directives and Starting Services and Depencency Injection

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -3,22 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
-    AppComponent, ShoppingListComponent, ShoppingListEditComponent, RecipesComponent, RecipeItemComponent, RecipeDetailComponent, HeaderComponent, RecipeListComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipesComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    DropdownDirective
   ],
-  imports: [
-    BrowserModule, FormsModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- recipe-book/src/app/shared/dropdown.directive.ts
+++ recipe-book/src/app/shared/dropdown.directive.ts
@@ -1,8 +1,34 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appDropdown]'
+  selector: '[appDropdown]',
 })
 export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  // @HostBinding('class') classes: string;
+  // flag: boolean = false;
 
+  constructor(private elRef: ElementRef) {}
+
+  @HostListener('click') toogleOpen() {
+    // this.isOpen = !this.isOpen;
+    // if (this.flag) {
+    //   this.classes = 'btn-group open';
+    // } else {
+    //   this.classes = 'btn-group';
+    // }
+    // this.flag = !this.flag;
+  }
+
+  @HostListener("document:click", ["$event"]) toggled(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false
+    console.log(event)
+  }
 }
+
+
